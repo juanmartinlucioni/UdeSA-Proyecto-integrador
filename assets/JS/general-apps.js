bf5f4c29c020cc76526d4e74281649a9c99d2525
MAyerzaUdesa <mayerza@udesa.edu.ar>
2019-10-22 14:01:28 -0300

    Search beta 2.0

---
+++ assets/JS/general-apps.js
@@ -0,0 +1,22 @@
+// Animaciones Menu Hamburguesa
+const hamburger = document.querySelector(".hamburger");
+const navlinks = document.querySelector(".nav_links");
+const links = document.querySelectorAll(".nav_links li");
+
+hamburger.addEventListener("click",()=> {
+  navlinks.classList.toggle("open");
+  navlinks.classList.toggle("is-visible");
+  links.forEach(link =>{
+    link.classList.toggle("fade");
+  });
+});
+
+//Animaciones Search Bar
+const searchbar = document.querySelector(".searchbar");
+const searchbox = document.querySelector(".searchbox");
+const searchicon = document.querySelector(".fa-search");
+
+searchicon.addEventListener("click", ()=> {
+  searchbar.classList.toggle("expand");
+  searchbox.classList.toggle("search-flex");
+});

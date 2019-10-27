512369fc874791db0e72098958e543232a9842bd
MatuAyerza <matu.ayerza@gmail.com>
2019-10-27 16:07:02 -0300

    Buscador beta, head completo htmls

--- assets/JS/api-general.js
+++ assets/JS/api-general.js
@@ -1,13 +1,28 @@
-// Trending
-fetch("https://api.themoviedb.org/3/trending/tv/day?api_key=2d4fd4d7daaa410f13903dbc540ca5d4")
-  .then(function(respuesta){
-    return respuesta.json();
-  })
-  .then(function(data){
-    var seriesData = data.results;
-    for(var i=0; i <6; i++){
-      document.querySelector(".series-names").innerHTML += "<li>"+seriesData[i].name+"</li>";
-      var seriesPosters = document.querySelector(".series-posters")
-      seriesPosters.innerHTML += "<li><img class='series-img' src='https://image.tmdb.org/t/p/original/" + seriesData[i].poster_path + "' alt=''></li>" 
-    }
-  })
+// API KEY 2d4fd4d7daaa410f13903dbc540ca5d4
+const apiKey = "2d4fd4d7daaa410f13903dbc540ca5d4";
+
+// Search Results
+var searchBaseUrl = "https://api.themoviedb.org/3/search/tv?api_key="
+
+var searchBar = document.getElementById("searchbar");
+function runSearch(keyword) {
+  let url = "".concat(searchBaseUrl, apiKey, "&query=", keyword)
+  document.getElementById("results-list").innerHTML = "";
+  console.log(url);
+  fetch(url)
+    .then(result => result.json())
+    .then(function (data) {
+      var seriesSearchResult = data.results;
+      for (var i = 0; i < seriesSearchResult.length; i++) {
+        document.getElementById("results-list").innerHTML += "<ul class='result result"+i+"'><li>" + seriesSearchResult[i].name + "</li>" + "<li><img class='series-img' src='https://image.tmdb.org/t/p/w185/" + seriesSearchResult[i].poster_path + "' alt=''></li></ul>"
+      }
+    })
+}
+searchBar.onkeypress = function (event) {
+  var key = event.key;
+  if (key == "Enter") {
+    var searchQueryStr = searchBar.value;
+    var searchQuery = searchQueryStr.replace(" ", "+")
+    runSearch(searchQuery);
+  }
+}

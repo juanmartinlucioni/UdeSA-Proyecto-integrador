fa9fe4569ff72f4882bac0f3a25f7d9fcf99483c
MatuAyerza <matu.ayerza@gmail.com>
2019-10-28 11:44:39 -0300

    Search 2.0

--- assets/JS/api-general.js
+++ assets/JS/api-general.js
@@ -8,21 +8,28 @@ var searchBar = document.getElementById("searchbar");
 function runSearch(keyword) {
   let url = "".concat(searchBaseUrl, apiKey, "&query=", keyword)
   document.getElementById("results-list").innerHTML = "";
-  console.log(url);
+  console.log(url); //ELIMINAR
   fetch(url)
     .then(result => result.json())
     .then(function (data) {
       var seriesSearchResult = data.results;
       for (var i = 0; i < seriesSearchResult.length; i++) {
+        if (seriesSearchResult[i].poster_path !== null) {
         document.getElementById("results-list").innerHTML += "<ul class='result result"+i+"'><li>" + seriesSearchResult[i].name + "</li>" + "<li><img class='series-img' src='https://image.tmdb.org/t/p/w185/" + seriesSearchResult[i].poster_path + "' alt=''></li></ul>"
+      } else {
+        document.getElementById("results-list").innerHTML += "<ul class='result result" + i + "'><li>" + seriesSearchResult[i].name + "</li>" + "<li><img class='series-img' src='/assets/IMG/Zseries1.png' alt=''></li></ul>"
       }
+    }
+      console.log(seriesSearchResult) //ELIMINAR
     })
 }
 searchBar.onkeypress = function (event) {
   var key = event.key;
   if (key == "Enter") {
     var searchQueryStr = searchBar.value;
-    var searchQuery = searchQueryStr.replace(" ", "+")
-    runSearch(searchQuery);
+    var searchQueryFix = searchQueryStr.replace(" ", "+")
+    localStorage.setItem("searchQuery", searchQueryFix);
+    // location.href = "page4-resultados-del-buscador.html?query=" + localStorage.getItem("searchQuery") WIP
+    runSearch(localStorage.getItem("searchQuery"));
   }
 }

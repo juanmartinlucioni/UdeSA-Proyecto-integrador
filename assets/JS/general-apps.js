6ec977feb56d5770308530d9bbb5a26d43efd47e
juanmartinlucioni <jlucioni@udesa.edu.ar>
2019-10-29 11:31:27 -0300

    Register + Login ( 100% working )

--- assets/JS/general-apps.js
+++ assets/JS/general-apps.js
@@ -71,40 +71,66 @@ window.addEventListener("click",function(event) {
 
 document.querySelector(".register-modal").addEventListener("submit",function(event){
   event.preventDefault()
-  // var nuevoUsuario= document.getElementById("reg-user").value
-  // console.log(nuevoUsuario);
-
+  var nuevoUsuario= document.getElementById("reg-user").value
+  console.log(nuevoUsuario); // eliminar
   var users = JSON.parse(localStorage.getItem('Users')) || [];
-  var userData = [{
-      Username: document.getElementById("reg-user").value
-    },
-    {
-      Email: document.getElementById("reg-email").value
-    },
-    {
-      Password: document.getElementById("reg-psw").value
+  console.log(users)
+  if (users.filter(e => e.username === nuevoUsuario).length > 0) {
+     return console.log("Este usuario ya esta registrado");
+  }
+  else {
+    var userData = {
+      username: document.getElementById("reg-user").value,
+      email: document.getElementById("reg-email").value,
+      password: document.getElementById("reg-psw").value
     }
-  ];
-  users.push(userData);
-  localStorage.setItem('Users', JSON.stringify(users));
-  document.getElementById("register-modal").style.display="none"
-  document.getElementById("login-modal").style.display="block"
-console.log(users);
+    users.push(userData);
+    localStorage.setItem('Users', JSON.stringify(users));
+    document.getElementById("register-modal").style.display = "none"
+    document.getElementById("login-modal").style.display = "block"
+    console.log(users);
+    return alert(`New user ${nuevoUsuario} now registered!`);
+    }
+  }
+)
+// al poner la contrasena mal, me dijo que el usuario no existe, osea no loggea, necesito una condicon que cuando exista usuario pero psw != notificacion " contrasena mal
 
-//   if (nuevoUsuario === usuario) {
-//     // innerHTML capturo login y lo remplazo
-//     console.log("son iguales");
-//     document.getElementById('login').innerText=nuevoUsuario
-
-//   }
-//   else {
-//     // guardar dato en local storage, set item
-//     registerUser(nuevoUsuario)
-//     console.log("usuario registrado");
-//     function registerUser(Usuario){
-//       window.localStorage.setItem("usuario", nuevoUsuario);
-//       return `New user ${nuevoUsuario} now registered!`;
-//   }
-// }
+// Log in with User
+document.querySelector(".login-modal").addEventListener("submit", function (event) {
+  event.preventDefault()
+  var users = JSON.parse(localStorage.getItem('Users')) || [];
+  console.log(users)
+  var usurarioIngresado = document.getElementById("login-user").value
+  console.log(usurarioIngresado);
+  var passwordIngresado = document.getElementById("login-psw").value
+  console.log(passwordIngresado);
+  if ((users.filter(e => e.username === usurarioIngresado).length > 0) && (users.filter(e => e.password === passwordIngresado).length > 0)){
+  document.querySelector(".login-button").innerHTML= usurarioIngresado
+  document.getElementById("login-modal").style.display = "none"
+  UIkit.notification({
+    message: 'Bienvenido '+ usurarioIngresado + "!",
+    status: 'primary',
+    pos: 'bottom-left',
+    timeout: 5000
+  });
+  }
+  else if((users.filter(e => e.username === usurarioIngresado).length > 0) && (users.filter(e => e.password !== passwordIngresado).length > 0)){
+    UIkit.notification({
+      message: 'Contraseña incorrecta!',
+      status: 'danger',
+      pos: 'bottom-left',
+      timeout: 5000
+    });
+  }
+  else {
+    UIkit.notification({
+      message: usurarioIngresado+' no existe, porfavor registrese!',
+      status: 'warning',
+      pos: 'bottom-left',
+      timeout: 5000
+    });
+    document.getElementById("login-modal").style.display = "none"
+    document.getElementById("register-modal").style.display = "block"
+  }
+ 
 })
-    

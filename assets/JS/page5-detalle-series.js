636ffb6cbf5bf8f230006ebd50286eb30a142a82
jim-rattagan <jim.rattagan123@gmail.com>
2019-11-07 00:27:07 -0300

    cambios a detalle series

--- assets/JS/page5-detalle-series.js
+++ assets/JS/page5-detalle-series.js
@@ -15,14 +15,23 @@ window.addEventListener("load", function () {
                         poster = informacion.poster_path
                         posterUrl = 'https://image.tmdb.org/t/p/w400/'
                         image = posterUrl + poster
-                        genres = informacion.genres
+                        genresArray= []
                         language = informacion.original_language
                         sinopsis = informacion.overview
                         releaseDate = informacion.first_air_date
                         trailer = informacion.video
                         average = informacion.vote_average
+                        console.log(informacion)
+                        for (var i = 0; i < informacion.genres.length; i++) {
+                            var genres = informacion.genres[i].name
+                            genresArray.push(genres)
+                        }
+                       
+                        console.log(genres)
+                        console.log(genresArray)
+
                         if (trailer == false) {
-                            console.log("holas");
+                    
                             
                         };
                         console.log(informacion)
@@ -33,6 +42,10 @@ window.addEventListener("load", function () {
                             <img src="${image}" alt="">
                             <p id="p_rating"><strong>Rating:</strong> <span>${average} / 10 </span> </p>
                             <p><strong>First air date:</strong> <span>${releaseDate}</span></p>
+                            <h2>Generos<h2>
+                            <li>${genresArray}</li>
+                            <h2>Idiomas</h2>
+                            <p>${language}</p>
                             <h2>Sinopsis</h2>
                             <p>${sinopsis}</p>
                             <h2>Trailer</h2>
@@ -41,4 +54,66 @@ window.addEventListener("load", function () {
                 
                 
                 // "<h1>${title}</h1><img src="${posterURL}" alt=""><h2>Sinopsis</h2><p>${sinopsis}</p><h2>trailer</h2><iframe src="${trailer}" frameborder="0"></iframe>"
-                    })})
+                    })
+                        console.log(id)
+                        var recoUrl = "https://api.themoviedb.org/3/tv/"+ id +"/similar?api_key=" + apiKey + "&language=en-US&query&page=1"
+                        console.log(recoUrl);
+                        fetch(recoUrl)
+                            .then(function (respuesta) {
+                                 return respuesta.json()
+                            })
+                            .then(function (informacion) {
+                                var title
+                                var poster
+                                var posterUrl
+                                var image
+                                var id
+                                var average
+                                var releaseDate
+                                for (var i = 0; i < informacion.results.length; i++) {
+                                    id = informacion.results[i].id
+                                    title = informacion.results[i].name
+                                    poster = informacion.results[i].poster_path
+                                    posterUrl = 'https://image.tmdb.org/t/p/original/'
+                                    image = posterUrl + poster
+                                    average = informacion.results[i].vote_average
+                                    releaseDate = informacion.results[i].first_air_date;
+                
+                                    var listadoRecomendadas = document.querySelector(".listado-series-recomendadas")
+                                    listadoRecomendadas.innerHTML +=  `<div class="series">
+                                    <div class="overlay">
+                                    <div class="addBtn"><span><a href=""><i class="material-icons heart" id="fav-icon" onclick="favorite(${id});return false">favorite</i></a></span></div>
+                                    <div class="serie">
+                                        <h2>${title}</h2>
+                                        <p id="p_rating"><strong>Rating:</strong> <span>${average} / 10 </span> </p>
+                                        <p><strong>First air date:</strong> <span>${releaseDate}</span></p>
+                                        <a id="detalles" onclick="serieSelected('${id}')" href="page5-detalle-series.html">Detalle</a>
+                                    </div>
+                                    </div>
+                                    <div class="Imagenes">
+                                        <img src="${image}" alt="">
+                                    </div>
+                                    </div>`
+                                }
+                                 
+                    
+
+})
+// modal recommended
+var recoModal = document.getElementById("reco-modal");
+window.onclick = function(event) {
+     if (event.target == recoModal) {
+         logModal.style.display = "none";
+     }
+ }
+
+var recoBtn = document.querySelector(".reco-button");
+  recoBtn.onclick = function() {
+  recoModal.style.display = "block";
+ }
+
+// var closeRecoModal = document.querySelector(".close-reco");
+//   closeRecoModal.onclick = function() {
+//   recoModal.style.display = "none";
+//  }
+})
